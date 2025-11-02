c7ce2b746130dd1636f2ca969a272ae877ebd6b9
Solace <monkdev9@gmail.com>
2025-11-02 22:19:18 +0000

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,18 +1,18 @@
 import { connectAuthDB } from '@/lib/db';
-import { Schema, model } from 'mongoose';
+import { Schema, model, Model } from 'mongoose';
 
 const userSchema = new Schema({
-  email: { type: String, unique: true },
-  username: { type: String, unique: true },
-  password: String,
+  email: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   globalRole: { type: String, default: 'user' },
   subscriptionPlan: { type: String, default: 'free' },
   ip_psi: String,
 });
 
-let UserModel: any;
+let UserModel: Model<any>;
 
-export default async function getUserModel() {
+export default async function getUserModel(): Promise<Model<any>> {
   if (!UserModel) {
     const db = await connectAuthDB();
     UserModel = db.model('User', userSchema);
